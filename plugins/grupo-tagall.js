0469e084a522052f598c10fab8da1ead08ab67ff
Charliexit <135475919+Charliexit@users.noreply.github.com>
2023-06-04 16:46:01 -0500

    Update grupo-tagall.js

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -5,11 +5,11 @@ throw false
 }
 let vn = './media/Invocar.mp3'
 let pesan = args.join` `
-let oi = `*Bot creado por Charlie* ${pesan}`
-let teks = `〔 *${wm}* 〕\n *anotense ctmr , no sean famtasmas😤* \n\n ${oi}\n\n *𝙀𝙩𝙞𝙦𝙪𝙚𝙩𝙖𝙨:*\n`
+let oi = `*Bot de @kzu.ffx* ${pesan}`
+let teks = `〔 *${wm}* 〕\n *Kenny, les pide que despierten fantasmas* \n\n ${oi}\n\n *𝙀𝙩𝙞𝙦𝙪𝙚𝙩𝙖𝙨:*\n`
 for (let mem of participants) {
-teks += `🤖 @${mem.id.split('@')[0]}\n`}
-teks += `└ @ventas.debot_charlie\n\n*<3*`
+teks += `🌷 @${mem.id.split('@')[0]}\n`}
+teks += `└ @kzu.ffx\n\n*<3*`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 conn.sendFile(m.chat, vn, 'Invocar.mp3', null, m, true, { type: 'audioMessage', ptt: true, sendEphemeral: true })
 }
