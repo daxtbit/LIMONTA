c4bc76c36300818b767c59a2ee62332d3ce64325
daxtbit <156219632+daxtbit@users.noreply.github.com>
2024-01-10 13:19:12 -0300

    Update grupo-tagall.js

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -6,10 +6,10 @@ throw false
 let vn = './media/Invocar.mp3'
 let pesan = args.join` `
 let oi = `*LIMONCITABB* ${pesan}`
-let teks = `〔 *${wm}* 〕\n *𝙇𝙞𝙢𝙤𝙣𝙞𝙩𝙖 𝙡𝙤𝙨 𝙞𝙣𝙫𝙤𝙘𝙖 𝙥𝙡𝙖𝙣𝙩𝙖𝙨* \n\n ${oi}\n\n *𝙀𝙩𝙞𝙦𝙪𝙚𝙩𝙖𝙨:*\n`
+let teks = `〔 *${wm}* 〕\n *𝙙𝙖𝙭𝙩 𝙞𝙣𝙫𝙤𝙘𝙖 𝙖 𝙥𝙡𝙖𝙣𝙩𝙖𝙨* \n\n ${oi}\n\n *𝙀𝙩𝙞𝙦𝙪𝙚𝙩𝙖𝙨:*\n`
 for (let mem of participants) {
-teks += `🌷 @${mem.id.split('@')[0]}\n`}
-teks += `└ @Ricardo_limonta\n\n*<3*`
+teks += `🦦 @${mem.id.split('@')[0]}\n`}
+teks += `└ @daxtbl\n\n*<3*`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 conn.sendFile(m.chat, vn, 'Invocar.mp3', null, m, true, { type: 'audioMessage', ptt: true, sendEphemeral: true })
 }
